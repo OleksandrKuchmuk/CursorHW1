73e9a722feb6c2d8efd6bc23b82fb09335523294
Oleksandr Kuchmuk <okuchmuk@gmail.com>
2021-12-16 10:43:47 +0200

    fixed the issue with division by zero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	 int a = 47;
-	 int b = 92;
-     int X = 17;
-	 double x = 5.23;
-	 double y = 13.58;
-     float w = 27.22f;
-     float e = 17.68f;
+        int a = 47;
+        int b = 92;
+        int X = 17;
+        double x = 5.23;
+        double y = 13.58;
+        float w = 27.22f;
+        float e = 0;
 
-     double result1;
-     double result2;
-     double result3;
-     float result4;
+        double result1;
+        double result2;
+        double result3;
+        double result4;
 
-        result1 = x+y-a/2*(x+w);
-        result2 = a*(x+x)/y-a*3;
-        result3 = a+b/x-y;
-        result4 = X-e/2%e+1;
+        result1 = x + y - a / 2 * (x + w);
+        result2 = a * (x + x) / y - a * 3;
+        result3 = a + b / x - y;
+        if (e != 0) {
+            result4 = X - e / 2 % e + 1;
+        } else throw new ArithmeticException("e=0, cannot be divided by zero");
+
 
         System.out.println(result1);
         System.out.println(result2);
